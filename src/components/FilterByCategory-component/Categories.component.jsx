30f73681a7aee43fe1c7cc4d51476ca2cefc3a58
Marinka Szilveszter <szilvesztermarinka4@gmail.com>
2023-01-30 23:05:16 +0100

    Categories render by map function

--- src/components/FilterByCategory-component/Categories.component.jsx
+++ src/components/FilterByCategory-component/Categories.component.jsx
@@ -1,38 +1,31 @@
 import React from "react";
+import categories from "../../data/categories.js";
 
-const Categories = () => {
-  return (
-    <div className="filter-wrapper">
-      <div className="filter-item active">
-        <img
-          src="https://media.mcdonalds.hu/storage/65/92/mcmenu.png"
-          alt="osszes"
-        />
-        <h3>Összes</h3>
-      </div>
-      <div className="filter-item">
-        <img
-          src="https://media.mcdonalds.hu/storage/52/78/kategoria-szendvics-category.png"
-          alt="burgerek"
-        />
-        <h3>Burgerek</h3>
-      </div>
-      <div className="filter-item">
-        <img
-          src="https://media.mcdonalds.hu/storage/54/44/dig_001_weboldal_kategoria_desszert_130x106px-category.png"
-          alt="desszertek"
-        />
-        <h3>Desszertek</h3>
-      </div>
-      <div className="filter-item">
-        <img
-          src="https://media.mcdonalds.hu/storage/65/96/kategoria_uditok-category.png"
-          alt="uditok"
-        />
-        <h3>Üdítők</h3>
-      </div>
-    </div>
-  );
+class Categories extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeCategoryFilterid: '1'
+    }
+  }
+
+  render() {
+    return (
+      <div className="filter-wrapper">
+        {categories.map((category) => {
+          return(
+            <div className="filter-item" key={category.id}>
+            <img
+              src={category.image}
+              alt={category.name}
+            />
+            <h3>{category.name}</h3>
+          </div>
+          )
+        })}
+        </div>
+    );
+  }
 };
 
 export default Categories;
---
+++ src/data/categories.js
@@ -0,0 +1,24 @@
+const categories = [
+    {
+        id: 1,
+        name: 'összes',
+        image: 'https://media.mcdonalds.hu/storage/65/92/mcmenu.png',
+    },
+    {
+        id: 2,
+        name: 'burgerek',
+        image: 'https://media.mcdonalds.hu/storage/52/78/kategoria-szendvics-category.png',
+    },
+    {
+        id: 3,
+        name: 'desszertek',
+        image: 'https://media.mcdonalds.hu/storage/54/44/dig_001_weboldal_kategoria_desszert_130x106px-category.png',  
+    },
+    {
+        id: 4,
+        name: 'üdítők',
+        image: 'https://media.mcdonalds.hu/storage/65/96/kategoria_uditok-category.png',
+    },
+]
+
+export default categories;
